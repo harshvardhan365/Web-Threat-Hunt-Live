1c4fe3d5323624c3fd7076f51db07c93a675bb1e
Harshvardhan <ekboteharshvardhan@gmail.com>
2023-06-21 12:44:11 +0530

    updated mongo

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,11 @@
 const mongoose = require(`mongoose`);
-mongoose.connect(`mongodb://0.0.0.0:27017/WTH`,{
+mongoose.connect(`mongodb+srv://admin:BB2uZ0YXk0559tUV@cluster0.dmdls8s.mongodb.net/?retryWrites=true&w=majority`,
+{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     // useCreateIndex:true
 }).then(()=>{
-    console.log(`Mongodb is connected`);
+    console.log(`Cloud Database is connected successfully !!!`);
 }).catch((error)=>{
     console.log(`Error occur due to this `,error);
 })
